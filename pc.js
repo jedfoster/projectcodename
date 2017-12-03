61201f7b56d28b0fe1b1586292458d33c3776083
Jed Foster <jed@jedfoster.com>
2017-12-03 14:34:24 -0800

    Code formatting fixes

--- pc.js
+++ pc.js
@@ -1,65 +1,60 @@
 var attributes = [
 	// Environ
-	"desert", "tundra", "mountain", "space", "field", "urban", "intergalactic",
+	'desert', 'tundra', 'mountain', 'space', 'field', 'urban', 'intergalactic',
 	// Stealth and cunning
-	"hidden", "covert", "uncanny", "scheming", "decisive",
+	'hidden', 'covert', 'uncanny', 'scheming', 'decisive',
 	// Volitility
-	"rowdy", "dangerous", "explosive", "threatening",
+	'rowdy', 'dangerous', 'explosive', 'threatening',
 	// Needs correction
-	"bad", "unnecessary", "unknown", "unexpected", "rude", "nefarious",
+	'bad', 'unnecessary', 'unknown', 'unexpected', 'rude', 'nefarious',
 	// Organic Gems and materials
-	"amber", "bone", "coral", "ivory", "pearl", 
+	'amber', 'bone', 'coral', 'ivory', 'pearl',
 	// Regular Gems
-	"diamond", "ruby", "jade",
+	'diamond', 'ruby', 'jade',
   // Cloth Materials
-  "cotton", "linen", "rubber", "silk", "straw", "woolen",
+  'cotton', 'linen', 'rubber', 'silk', 'straw', 'woolen',
 	// Colors
-  "red", "orange", "yellow", "green", "blue", "violet", "beige", "crimson", "indigo", "creme",
+  'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'beige', 'crimson', 'indigo', 'creme',
   // Adjectives
-  "angry", "beautiful", "brave", "careful", "careless", "clever", "crazy", "cute", "dangerous", "exciting", "famous", "friendly", "happy", "interesting", "lucky", "popular", "rich", "sad", "thin", "unlucky", 
+  'angry', 'beautiful', 'brave', 'careful', 'careless', 'clever', 'crazy', 'cute', 'dangerous', 'exciting', 'famous', 'friendly', 'happy', 'interesting', 'lucky', 'popular', 'rich', 'thin', 'unlucky',
   // Light
-  "blazing", "glittering", "radiant", "glowing", "brilliant", "luminous", "incandescent",
-]
+  'blazing', 'glittering', 'radiant', 'glowing', 'brilliant', 'luminous', 'incandescent',
+];
 
 var objects = [
 	// Large cats
-	"panther", "lion", "leopard", "house cat",
+	'panther', 'lion', 'leopard',
 	// Snakes
-	"viper", "cobra",
+	'viper', 'cobra',
 	// Other predators
-	"grizzly", "jackal", "falcon",
+	'grizzly', 'jackal', 'falcon',
 	// Prey
-	"zebra", "elk", "moose", "llama", "alpaca",
+	'zebra', 'elk', 'moose', 'llama', 'alpaca', 'goat',
 	// Occupations
-	"wizard", "cleric", "pilot",
+	'wizard', 'cleric', 'pilot',
 	// Sea life
-	"octopus", "lobster", "hammerhead", "orca",
+	'octopus', 'lobster', 'hammerhead', 'orca',
 	// Weather
-	"storm", "thunder", "lightning", "rain", "hail", "sun", "snow",
-]
+	'storm', 'thunder', 'lightning', 'rain', 'hail', 'sun', 'snow',
+];
 
-var type = "PROJECT";
-var codename = "CODENAME";
+var type = 'PROJECT';
+// var codename = 'CODENAME';
 
-function createCodename()
-{
-	var f = attributes[Math.floor(Math.random() * attributes.length)].toUpperCase();
-	var l = objects[Math.floor(Math.random() * objects.length)].toUpperCase();
-	codename = f + " " + l;
+function createCodename() {
+	var attribute = attributes[Math.floor(Math.random() * attributes.length)].toUpperCase();
+	var object = objects[Math.floor(Math.random() * objects.length)].toUpperCase();
+	var codename = attribute + ' ' + object;
 
 	$('#codename').text(codename);
 	updateTwitterButton();
 }
 
-function switchType()
-{
-	if (type == "PROJECT")
-	{
+function switchType() {
+	if (type == "PROJECT") {
 		type = "OPERATION";
 		$('#switchType').text("It's a project.");
-	}
-	else
-	{
+	} else {
 		type = "PROJECT";
 		$('#switchType').text("It's an operation.");
 	}
@@ -68,17 +63,15 @@ function switchType()
 	updateTwitterButton();
 }
 
-function updateTwitterButton()
-{
+function updateTwitterButton() {
 	$('#twitter').replaceWith("<a href=\"https://twitter.com/share\" id=\"twitter\" class=\"twitter-share-button\" data-via=\"zachwlewis\" data-url=\"http://projectcodename.com\">Tweet</a>");
 	$('#twitter').attr('data-text', type + ": " + codename + " is a go.");
 
-	if(typeof(twttr) !== 'undefined')
-	{
+	if(typeof(twttr) !== 'undefined') {
 		twttr.widgets.load();
 	}
 }
 
-createCodename();
+// createCodename();
 $('#newProject').click(createCodename);
 $('#switchType').click(switchType);
